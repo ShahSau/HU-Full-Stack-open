a4d9287d856092712f9837f07bd060b6ea33d74b
shahriar <shahriarksaurov@gmail.com>
2021-08-30 10:48:33 +0300

    1.8: unicafe step3

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  //creating event handlers
   const increaseGood = () => {
     setGood(good + 1);
   };
@@ -15,19 +16,34 @@ const App = () => {
   const increaseNeutral = () => {
     setNeutral(neutral + 1);
   };
+
+  // creating statistics component
+  const Statistics = (props) => {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.good + props.bad + props.neutral}</p>
+        <p>
+          average{" "}
+          {(props.good - props.bad) / (props.good + props.bad + props.neutral)}
+        </p>
+        <p>
+          positive{" "}
+          {(props.good / (props.good + props.bad + props.neutral)) * 100}%
+        </p>
+      </div>
+    );
+  };
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => increaseGood()}>good</button>
       <button onClick={() => increaseNeutral()}>neutral</button>
       <button onClick={() => increaseBad()}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {(good - bad) / (good + bad + neutral)}</p>
-      <p>positive {(good / (good + bad + neutral)) * 100}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };

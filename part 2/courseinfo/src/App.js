41c03d9938612e576133fb0a49a5d36769d6d355
shahriar <shahriarksaurov@gmail.com>
2021-08-31 10:08:00 +0300

    2.2: Course information step7
    created a new folder called courseinfo in part 2

---
+++ part 2/courseinfo/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Course = ({ course }) => {
+  let total = 0;
+  course.parts.map((item) => {
+    return (total += item.exercises);
+  });
+  return (
+    <div>
+      <h2>{course.name}</h2>
+      {course.parts.map((item) => (
+        <p key={item.id}>
+          {item.name} {item.exercises}
+        </p>
+      ))}
+      <p key={Math.random()}>
+        <strong>total of {total} exercise</strong>
+      </p>
+    </div>
+  );
+};
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+export default App;

639bfbd1541072cc9c470d53d8bb23f78077199c
ShahSau <shahriarksaurov@gmail.com>
2021-10-05 23:36:13 +0300

    4.8: Blog list tests, step1

--- part 4/BlogPost/app.js
+++ part 4/BlogPost/app.js
@@ -6,7 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-
+require('express-async-errors')
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)

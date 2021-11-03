81194ecfd76b0681ec65a20803552d0962c8e6ce
ShahSau <shahriarksaurov@gmail.com>
2021-11-03 16:48:54 +0200

    4.20 *: bloglist expansion, step8

--- part 4/BlogPost/controllers/blogs.js
+++ part 4/BlogPost/controllers/blogs.js
@@ -17,22 +17,15 @@ blogsRouter.get("/:id", async (request, response) => {
     response.status(404).end();
   }
 });
-const getTokenFrom = request =>{
-  const authorization = request.get('authorization')
-  if(authorization && authorization.toLowerCase().startsWith('bearer ')){
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   if (body.title === undefined && body.url === undefined) {
     response.status(400).end();
   } else {
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if(!token || !decodedToken.id){
+    
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if(!request.token || !decodedToken.id){
       return response.status(401).json({
         error: "token missing or invalid"
       })

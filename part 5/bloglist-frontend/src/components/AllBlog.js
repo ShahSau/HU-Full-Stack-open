e69dc0f8df5aea8d58d7860c8e99b6e3d8281d7c
ShahSau <shahriarksaurov@gmail.com>
2021-11-06 11:21:45 +0200

    5.9*: Blog list frontend, step9

--- part 5/bloglist-frontend/src/components/AllBlog.js
+++ part 5/bloglist-frontend/src/components/AllBlog.js
@@ -1,18 +1,16 @@
 import React from "react";
-import Blog from './Blog'
+import Blog from "./Blog";
 
-const allBlogs =({
-blogs,
-likeButton
-})=>{
-    return (
-        <div>
-        {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} likeButton={likeButton}/>
-          ))}
-          </div>
-    )
-}
-
+const allBlogs = ({ blogs, likeButton }) => {
+  return (
+    <div>
+      {blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map((blog) => (
+        <Blog key={blog.id} blog={blog} likeButton={likeButton} />
+      ))}
+    </div>
+  );
+};
 
 export default allBlogs;
